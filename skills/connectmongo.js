44920a6f30c617f0dd5f2c6d670118664eafddc9
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-12 15:29:23 +0700

    C:/Users/Peter/AppData/Local/GitHub/PortableGit_f02737a78695063deace08e96d5042710d3e32db/translate building - incremental

--- skills/connectmongo.js
+++ skills/connectmongo.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 // Connect to the db before tests run
 
 //Connect to mongodb
-module.exports = function() {
+module.exports = function(controller) {
   mongoose.connect('mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASS + '@ds127063.mlab.com:27063/oombaw');
   mongoose.connection
     .once('open', function() {
