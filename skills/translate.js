9b52613c18e33e829ce344cabafc44d3706a33c6
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-12 16:52:54 +0700

    C:/Users/Peter/AppData/Local/GitHub/PortableGit_f02737a78695063deace08e96d5042710d3e32db/translate saving preferences - incremental

--- skills/translate.js
+++ skills/translate.js
@@ -15,7 +15,7 @@ module.exports = function(controller) {
       if (userObj.hasOwnProperty('translateTo')) {
         bot.whisper(message, "translating...");
         let original = message.text;
-
+        translateWord(currentUser, original);
       } else {
         let original = message.text;
         bot.whisper(message, {
@@ -65,12 +65,33 @@ module.exports = function(controller) {
         });
 
         controller.on('interactive_message_callback', function(bot, message) {
-          bot.reply(message, 'preferences saved ' + message.callback_id);
-          oombawDB.addUserPref(message, message.text);
+          bot.whisper(message, 'preferences saved ' + message.callback_id);
+          oombawDB.addUserPref(message, message.text).then(currentUser => {
+            translateWord(currentUser, original);
+          });
         });
       }
     })
 
   });
+
+
+  function translateWord(currentUser, text) {
+    console.log(text);
+    translate(text, {
+        to: currentUser.translateTo
+      })
+      .then(res => {
+        res.original = text.toLowerCase();
+        res.translated = res.text.toLowerCase();
+        bot.whisper(res.original ': ' + res.translated);
+        //console.log(original + ' is ' + translated);
+        // console.log(res.text);
+        // => I speak English
+        // console.log(res.from.language.iso);
+        // => nl
+        //saveYesOrNo(currentUser, msg, res);
+      });
+  }
 
 }
