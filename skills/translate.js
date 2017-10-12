6dcc871c0b48d244b0365032ef9b47eeb3330106
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-12 14:56:42 +0700

    testing hearing of bot

---
+++ skills/translate.js
@@ -0,0 +1,15 @@
+module.exports = function(controller) {
+
+  // add event handlers to controller
+  // such as hears handlers that match triggers defined in code
+  // or controller.studio.before, validate, and after which tie into triggers
+  // defined in the Botkit Studio UI.
+  controller.hears('peter', ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+
+    bot.reply(message, 'Hello Peter.');
+    bot.reply(message, message);
+    bot.reply(message, bot);
+
+  });
+
+}
