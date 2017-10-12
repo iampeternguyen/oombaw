07fb23c739ef0bb6e6244a3ec3f1ea362da77db7
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-12 16:58:31 +0700

    C:/Users/Peter/AppData/Local/GitHub/PortableGit_f02737a78695063deace08e96d5042710d3e32db/translate saving preferences - incremental

--- skills/translate.js
+++ skills/translate.js
@@ -8,70 +8,70 @@ module.exports = function(controller) {
   // or controller.studio.before, validate, and after which tie into triggers
   // defined in the Botkit Studio UI.
   controller.on('slash_command', function(bot, message) {
-
-    // reply to slash command
-    oombawDB.checkAddUser(message).then(currentUser => {
-      let userObj = currentUser.toObject();
-      if (userObj.hasOwnProperty('translateTo')) {
-        bot.whisper(message, "translating...");
-        let original = message.text;
-        translateWord(currentUser, original, message);
-      } else {
-        let original = message.text;
-        bot.whisper(message, {
-          "text": "What language would you like to translate to?",
-          "response_type": "ephemeral",
-          "attachments": [{
-            //"text": "Choose a language to translate to",
-            "fallback": "",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "callback_id": "language_selection",
-            "actions": [{
-              "name": "language_choice",
-              "text": "Pick a language...",
-              "type": "select",
-              "options": [{
-                  "text": "Afrikaans",
-                  "value": "af"
-                },
-                {
-                  "text": "Albanian",
-                  "value": "sq"
-                },
-                {
-                  "text": "Arabic ",
-                  "value": "ar"
-                },
-                {
-                  "text": "Azerbaijani",
-                  "value": "az"
-                },
-                {
-                  "text": "Basque",
-                  "value": "eu"
-                },
-                {
-                  "text": "English",
-                  "value": "en"
-                },
-                {
-                  "text": "Vietnamese",
-                  "value": "vi"
-                }
-              ]
-            }]
-          }]
-        });
-
-        controller.on('interactive_message_callback', function(bot, message) {
-          bot.whisper(message, 'preferences saved ' + message.callback_id);
-          oombawDB.addUserPref(message, message.text).then(currentUser => {
-            translateWord(currentUser, original, message);
-          });
-        });
-      }
-    })
+    bot.replyPrivate(message, 'boopbeep')
+    // // reply to slash command
+    // oombawDB.checkAddUser(message).then(currentUser => {
+    //   let userObj = currentUser.toObject();
+    //   if (userObj.hasOwnProperty('translateTo')) {
+    //     bot.whisper(message, "translating...");
+    //     let original = message.text;
+    //     translateWord(currentUser, original, message);
+    //   } else {
+    //     let original = message.text;
+    //     bot.whisper(message, {
+    //       "text": "What language would you like to translate to?",
+    //       "response_type": "ephemeral",
+    //       "attachments": [{
+    //         //"text": "Choose a language to translate to",
+    //         "fallback": "",
+    //         "color": "#3AA3E3",
+    //         "attachment_type": "default",
+    //         "callback_id": "language_selection",
+    //         "actions": [{
+    //           "name": "language_choice",
+    //           "text": "Pick a language...",
+    //           "type": "select",
+    //           "options": [{
+    //               "text": "Afrikaans",
+    //               "value": "af"
+    //             },
+    //             {
+    //               "text": "Albanian",
+    //               "value": "sq"
+    //             },
+    //             {
+    //               "text": "Arabic ",
+    //               "value": "ar"
+    //             },
+    //             {
+    //               "text": "Azerbaijani",
+    //               "value": "az"
+    //             },
+    //             {
+    //               "text": "Basque",
+    //               "value": "eu"
+    //             },
+    //             {
+    //               "text": "English",
+    //               "value": "en"
+    //             },
+    //             {
+    //               "text": "Vietnamese",
+    //               "value": "vi"
+    //             }
+    //           ]
+    //         }]
+    //       }]
+    //     });
+    //
+    //     controller.on('interactive_message_callback', function(bot, message) {
+    //       bot.whisper(message, 'preferences saved ' + message.callback_id);
+    //       oombawDB.addUserPref(message, message.text).then(currentUser => {
+    //         translateWord(currentUser, original, message);
+    //       });
+    //     });
+    //   }
+    // })
 
   });
 
