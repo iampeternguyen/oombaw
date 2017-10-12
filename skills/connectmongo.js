1e93ca414d73708ee81a915f176fb46137edb37a
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-12 15:26:08 +0700

    C:/Users/Peter/AppData/Local/GitHub/PortableGit_f02737a78695063deace08e96d5042710d3e32db/translate building - incremental

---
+++ skills/connectmongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+//ES6 Promises
+mongoose.Promise = global.Promise;
+
+// Connect to the db before tests run
+
+//Connect to mongodb
+module.exports = function() {
+  mongoose.connect('mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PASS + '@ds127063.mlab.com:27063/oombaw');
+  mongoose.connection
+    .once('open', function() {
+      console.log('Connection has been made, now make fireworks...');
+    })
+    .on('error', function(error) {
+      console.log('Connection error: ', error);
+    });
+
+
+};
