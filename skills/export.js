f5263f358023b8d9d64704517911284dd55a85af
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-18 09:39:59 +0700

    WIP: export vocab list

--- skills/export.js
+++ skills/export.js
@@ -3,31 +3,34 @@ const oombawDB = require('../controllers/oombawDB');
 
 module.exports = function(controller) {
   controller.hears(['export'], 'direct_message,direct_mention', (bot, message) => {
-    let currentUser = oombawDB.getUser(message)
-    console.log(currentUser)
-    let vocabArray = []
-    for (i = 0; i < currentUser.vocabList.length; i++) {
-      vocabArray.push({
-        name: 'answer',
-        text: currentUser.vocabList[i].source,
-        type: 'button',
-        value: i
-      })
-    }
+    oombawDB.getUser(message).then(currentUser => {
+      console.log(currentUser)
+      let vocabArray = []
+      for (i = 0; i < currentUser.vocabList.length; i++) {
+        vocabArray.push({
+          name: 'answer',
+          text: currentUser.vocabList[i].source,
+          type: 'button',
+          value: i
+        })
+      }
 
-    bot.startConversation(message, (err, convo) => {
-      convo.ask({
-        user: currentUser.userID,
-        text: translatedMessage.text,
-        response_type: "ephemeral",
-        attachments: [{
-          text: "",
-          fallback: 'Yes or No?',
-          callback_id: 'yesno_callback',
-          actions: vocabArray
-        }]
+      bot.startConversation(message, (err, convo) => {
+        convo.ask({
+          user: currentUser.userID,
+          text: translatedMessage.text,
+          response_type: "ephemeral",
+          attachments: [{
+            text: "",
+            fallback: 'Yes or No?',
+            callback_id: 'yesno_callback',
+            actions: vocabArray
+          }]
+        })
       })
+
     })
+
   })
 
 }
