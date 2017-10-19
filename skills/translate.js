fc704460d1734a1d5230023503314c58d1ab4812
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-19 17:23:02 +0700

    WIP clean code test - if translateTO already exist

--- skills/translate.js
+++ skills/translate.js
@@ -93,7 +93,7 @@ module.exports = function(controller) {
 
 
   function translateWord(oombawUser, text) {
-    console.log(text)
+    console.log(oombawUser)
     return new Promise((resolve, reject) => {
       translate(text, {
         to: oombawUser.translateTo
