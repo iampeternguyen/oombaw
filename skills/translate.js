d59e9cd82249f4bc65ca45899a283ff8ad9355b8
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-20 21:37:53 +0700

    WIP clean code test - if no language preference

--- skills/translate.js
+++ skills/translate.js
@@ -11,7 +11,7 @@ module.exports = function(controller) {
 
     bot.startPrivateConversation(message, (err, convo) => {
       var msg = message;
-      console.log(convo);
+      convo.say('hello');
       oombawDB.checkAddUser(msg)
         .then(oombawUser => helper.checkLanguagePrefs(oombawUser, convo, controller))
         .then(oombawUser => translateWord(oombawUser, msg.text))
