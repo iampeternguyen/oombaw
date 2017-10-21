6f8a5544fc3daa8c339363c2a8158bc0c2e0b369
iampeternguyen <ptnguyen1987@gmail.com>
2017-10-21 16:08:07 +0700

    WIP building new user path

--- skills/translate.js
+++ skills/translate.js
@@ -6,39 +6,57 @@ module.exports = function(controller) {
 
   controller.on('slash_command', function(bot, message) {
     // reply to slash command
+
+    oombawDB.checkAddUser(message.team_id, message.user_id)
+      .then(oombawUser => helper.checkLanguagePrefs(oombawUser)
+        ? hasLangPrefsPath(oombawUser, message)
+        : setupPrefsPath(oombawUser, message))
+
+  });
+
+  function setupPrefsPath(oombawUser, message) {
+    // TODO reply to message and begin convo chain
+
+    bot.replyPrivate(message, "What language do you want to translate to?");
     // TODO create two pathways. One pathway responds to the request using replyPrivate with the translated message
     // TODO second pathway uses replyPrivate to ask a question first which starts a bot conversation 
     // to guide the user to setup their account
 
-    // bot.startConversation(message, (err, convo) => {
-    //   var msg = message;
-    //   convo.addMessage({
-    //     text: 'Hello let me ask you a question, then i will do something useful'
-    //   }, 'default');
-    //   convo.addQuestion({
-    //     text: 'What is your name?'
-    //   }, function(res, convo) {
-    //     // name has been collected...
-    //     convo.gotoThread('completed');
-    //   }, {
-    //     key: 'name'
-    //   }, 'default');
-    //   convo.addMessage({
-    //     text: 'done'
-    //   }, 'completed');
-    //console.log(message)
-    oombawDB.checkAddUser(message.team_id, message.user_id)
-      .then(oombawUser => helper.checkLanguagePrefs(oombawUser)
-        ? userExistsPath(oombawUser, message)
-        : newUserPath(oombawUser))
+    bot.startConversation(message, (err, convo) => {
+      convo.addMessage({
+        "text": "What language would you like to translate to?",
+        response_type: "ephemeral",
+        "attachments": [{
+          //"text": "Choose a language to translate to",
+          "fallback": "",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "callback_id": "language_selection",
+          "actions": [{
+            "name": "language_choice",
+            "text": "Pick a language...",
+            "type": "select",
+            "options": languages
+          }]
+        }]
+      }, 'default');
+      convo.addQuestion({
+        text: 'What is your name?'
+      }, function(res, convo) {
+        convo.gotoThread('completed');
+      }, {
+        key: 'name'
+      }, 'default');
+      convo.addMessage({
+        text: 'done'
+      }, 'completed');
+    });
 
-  });
 
-  function newUserPath(oombawUser) {
     console.log("wip");
   }
 
-  function userExistsPath(oombawUser, message) {
+  function hasLangPrefsPath(oombawUser, message) {
     translateWord(oombawUser, message.text)
       .then(oombawUser => {
         bot.replyPrivate(message, oombawUser.temp.original + ": " + oombawUser.temp.translated);
@@ -141,3 +159,422 @@ module.exports = function(controller) {
 
 }
 
+
+const languages = [
+  {
+    "text": "Afrikaans",
+    "value": "af"
+  },
+  {
+    "text": "Albanian",
+    "value": "sq"
+  },
+  {
+    "text": "Amharic",
+    "value": "am"
+  },
+  {
+    "text": "Arabic",
+    "value": "ar"
+  },
+  {
+    "text": "Armenian",
+    "value": "hy"
+  },
+  {
+    "text": "Azeerbaijani",
+    "value": "az"
+  },
+  {
+    "text": "Basque",
+    "value": "eu"
+  },
+  {
+    "text": "Belarusian",
+    "value": "be"
+  },
+  {
+    "text": "Bengali",
+    "value": "bn"
+  },
+  {
+    "text": "Bosnian",
+    "value": "bs"
+  },
+  {
+    "text": "Bulgarian",
+    "value": "bg"
+  },
+  {
+    "text": "Catalan",
+    "value": "ca"
+  },
+  {
+    "text": "Cebuano",
+    "value": "ceb (ISO-639-2)"
+  },
+  {
+    "text": "Chinese (Simplified)",
+    "value": "zh-CN"
+  },
+  {
+    "text": "Chinese (Traditional)",
+    "value": "zh-TW"
+  },
+  {
+    "text": "Corsican",
+    "value": "co"
+  },
+  {
+    "text": "Croatian",
+    "value": "hr"
+  },
+  {
+    "text": "Czech",
+    "value": "cs"
+  },
+  {
+    "text": "Danish",
+    "value": "da"
+  },
+  {
+    "text": "Dutch",
+    "value": "nl"
+  },
+  {
+    "text": "English",
+    "value": "en"
+  },
+  {
+    "text": "Esperanto",
+    "value": "eo"
+  },
+  {
+    "text": "Estonian",
+    "value": "et"
+  },
+  {
+    "text": "Finnish",
+    "value": "fi"
+  },
+  {
+    "text": "French",
+    "value": "fr"
+  },
+  {
+    "text": "Frisian",
+    "value": "fy"
+  },
+  {
+    "text": "Galician",
+    "value": "gl"
+  },
+  {
+    "text": "Georgian",
+    "value": "ka"
+  },
+  {
+    "text": "German",
+    "value": "de"
+  },
+  {
+    "text": "Greek",
+    "value": "el"
+  },
+  {
+    "text": "Gujarati",
+    "value": "gu"
+  },
+  {
+    "text": "Haitian Creole",
+    "value": "ht"
+  },
+  {
+    "text": "Hausa",
+    "value": "ha"
+  },
+  {
+    "text": "Hawaiian",
+    "value": "haw"
+  },
+  {
+    "text": "Hebrew",
+    "value": "iw"
+  },
+  {
+    "text": "Hindi",
+    "value": "hi"
+  },
+  {
+    "text": "Hmong",
+    "value": "hmn"
+  },
+  {
+    "text": "Hungarian",
+    "value": "hu"
+  },
+  {
+    "text": "Icelandic",
+    "value": "is"
+  },
+  {
+    "text": "Igbo",
+    "value": "ig"
+  },
+  {
+    "text": "Indonesian",
+    "value": "id"
+  },
+  {
+    "text": "Irish",
+    "value": "ga"
+  },
+  {
+    "text": "Italian",
+    "value": "it"
+  },
+  {
+    "text": "Japanese",
+    "value": "ja"
+  },
+  {
+    "text": "Javanese",
+    "value": "jw"
+  },
+  {
+    "text": "Kannada",
+    "value": "kn"
+  },
+  {
+    "text": "Kazakh",
+    "value": "kk"
+  },
+  {
+    "text": "Khmer",
+    "value": "km"
+  },
+  {
+    "text": "Korean",
+    "value": "ko"
+  },
+  {
+    "text": "Kurdish",
+    "value": "ku"
+  },
+  {
+    "text": "Kyrgyz",
+    "value": "ky"
+  },
+  {
+    "text": "Lao",
+    "value": "lo"
+  },
+  {
+    "text": "Latin",
+    "value": "la"
+  },
+  {
+    "text": "Latvian",
+    "value": "lv"
+  },
+  {
+    "text": "Lithuanian",
+    "value": "lt"
+  },
+  {
+    "text": "Luxembourgish",
+    "value": "lb"
+  },
+  {
+    "text": "Macedonian",
+    "value": "mk"
+  },
+  {
+    "text": "Malagasy",
+    "value": "mg"
+  },
+  {
+    "text": "Malay",
+    "value": "ms"
+  },
+  {
+    "text": "Malayalam",
+    "value": "ml"
+  },
+  {
+    "text": "Maltese",
+    "value": "mt"
+  },
+  {
+    "text": "Maori",
+    "value": "mi"
+  },
+  {
+    "text": "Marathi",
+    "value": "mr"
+  },
+  {
+    "text": "Mongolian",
+    "value": "mn"
+  },
+  {
+    "text": "Myanmar (Burmese)",
+    "value": "my"
+  },
+  {
+    "text": "Nepali",
+    "value": "ne"
+  },
+  {
+    "text": "Norwegian",
+    "value": "no"
+  },
+  {
+    "text": "Nyanja (Chichewa)",
+    "value": "ny"
+  },
+  {
+    "text": "Pashto",
+    "value": "ps"
+  },
+  {
+    "text": "Persian",
+    "value": "fa"
+  },
+  {
+    "text": "Polish",
+    "value": "pl"
+  },
+  {
+    "text": "Portuguese",
+    "value": "pt"
+  },
+  {
+    "text": "Punjabi",
+    "value": "pa"
+  },
+  {
+    "text": "Romanian",
+    "value": "ro"
+  },
+  {
+    "text": "Russian",
+    "value": "ru"
+  },
+  {
+    "text": "Samoan",
+    "value": "sm"
+  },
+  {
+    "text": "Scots Gaelic",
+    "value": "gd"
+  },
+  {
+    "text": "Serbian",
+    "value": "sr"
+  },
+  {
+    "text": "Sesotho",
+    "value": "st"
+  },
+  {
+    "text": "Shona",
+    "value": "sn"
+  },
+  {
+    "text": "Sindhi",
+    "value": "sd"
+  },
+  {
+    "text": "Sinhala (Sinhalese)",
+    "value": "si"
+  },
+  {
+    "text": "Slovak",
+    "value": "sk"
+  },
+  {
+    "text": "Slovenian",
+    "value": "sl"
+  },
+  {
+    "text": "Somali",
+    "value": "so"
+  },
+  {
+    "text": "Spanish",
+    "value": "es"
+  },
+  {
+    "text": "Sundanese",
+    "value": "su"
+  },
+  {
+    "text": "Swahili",
+    "value": "sw"
+  },
+  {
+    "text": "Swedish",
+    "value": "sv"
+  },
+  {
+    "text": "Tagalog (Filipino)",
+    "value": "tl"
+  },
+  {
+    "text": "Tajik",
+    "value": "tg"
+  },
+  {
+    "text": "Tamil",
+    "value": "ta"
+  },
+  {
+    "text": "Telugu",
+    "value": "te"
+  },
+  {
+    "text": "Thai",
+    "value": "th"
+  },
+  {
+    "text": "Turkish",
+    "value": "tr"
+  },
+  {
+    "text": "Ukrainian",
+    "value": "uk"
+  },
+  {
+    "text": "Urdu",
+    "value": "ur"
+  },
+  {
+    "text": "Uzbek",
+    "value": "uz"
+  },
+  {
+    "text": "Vietnamese",
+    "value": "vi"
+  },
+  {
+    "text": "Welsh",
+    "value": "cy"
+  },
+  {
+    "text": "Xhosa",
+    "value": "xh"
+  },
+  {
+    "text": "Yiddish",
+    "value": "yi"
+  },
+  {
+    "text": "Yoruba",
+    "value": "yo"
+  },
+  {
+    "text": "Zulu",
+    "value": "zu"
+  }
+];
